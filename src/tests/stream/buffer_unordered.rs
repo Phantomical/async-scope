c6ef46f1debf9443383b825701d95f5a4544e9b8
Phantomical <phantom@lynches.ca>
2024-03-04 22:25:32 -0800

    Move BufferUnordered tests to the tests module

--- src/tests/stream/buffer_unordered.rs
+++ src/tests/stream/buffer_unordered.rs
@@ -9,24 +9,55 @@ use crate::scope;
 use crate::stream::ScopedStreamExt;
 use crate::util::test::resolve;
 
+/// This tests that the results from futures submitted to
+/// scope_buffer_unordered can resolve out of order.
 #[tokio::test]
 async fn out_of_order() {
     let scope = scope!(|scope| {
         let (tx1, rx1) = oneshot::channel();
         let (tx2, rx2) = oneshot::channel();
 
-        let mut stream = pin!(stream::iter(vec![rx1, rx2]).scope_buffered(2, scope));
+        let mut stream = pin!(stream::iter(vec![rx1, rx2]).scope_buffer_unordered(2, scope));
 
+        // Futures can resolve out of order.
         tx2.send(1).unwrap();
-
-        // The second future has resolved but that shouldn't result in the stream
-        // returning a value.
-        assert_eq!(resolve(stream.next()).await, None);
+        assert_eq!(resolve(stream.next()).await, Some(Some(Ok(1))));
 
         tx1.send(2).unwrap();
-
         assert_eq!(resolve(stream.next()).await, Some(Some(Ok(2))));
+
+        assert_eq!(resolve(stream.next()).await, Some(None));
+    });
+
+    scope.await;
+}
+
+/// This test validates that futures outside of the buffered window are not
+/// executed until some space clears up in that window.
+#[tokio::test]
+async fn out_of_window() {
+    let scope = scope!(|scope| {
+        let (tx1, rx1) = oneshot::channel();
+        let (tx2, rx2) = oneshot::channel();
+        let (tx3, rx3) = oneshot::channel();
+
+        let mut stream = pin!(stream::iter(vec![rx1, rx2, rx3]).scope_buffer_unordered(2, scope));
+
+        // rx3 is outside of the buffer so it should not resolve
+        tx3.send(1).unwrap();
+        assert_eq!(resolve(stream.next()).await, None);
+
+        // Futures can resolve out of order.
+        tx2.send(2).unwrap();
+        assert_eq!(resolve(stream.next()).await, Some(Some(Ok(2))));
+
+        // And now the newly added future can resolve too.
         assert_eq!(resolve(stream.next()).await, Some(Some(Ok(1))));
+
+        tx1.send(3).unwrap();
+        assert_eq!(resolve(stream.next()).await, Some(Some(Ok(3))));
+
+        assert_eq!(resolve(stream.next()).await, Some(None));
     });
 
     scope.await;
@@ -53,7 +84,7 @@ async fn concurrent() {
 
         // These tasks need to be run concurrently to complete.
         let tasks = vec![pingpong(tx1, rx2, true), pingpong(tx2, rx1, false)];
-        let mut stream = pin!(stream::iter(tasks).scope_buffered(2, scope));
+        let mut stream = pin!(stream::iter(tasks).scope_buffer_unordered(2, scope));
 
         assert_matches!(resolve(stream.next()).await, Some(Some(_)));
         assert_matches!(resolve(stream.next()).await, Some(Some(_)));
